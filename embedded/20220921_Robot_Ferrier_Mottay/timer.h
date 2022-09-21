10d9553208d2b912a652ca2adf378c338a9e65e7
TABLE3 <touloniut@gmail.com>
2022-09-21 17:29:19 +0200

    fin timer

---
+++ embedded/20220921_Robot_Ferrier_Mottay/timer.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   timer.h
+ * Author: TP-EO-5
+ *
+ * Created on 21 septembre 2022, 16:22
+ */
+
+#ifndef TIMER_H
+#define	TIMER_H
+void InitTimer23( void ) ;
+void InitTimer1( void ) ;
+
+#endif	/* TIMER_H */
+
